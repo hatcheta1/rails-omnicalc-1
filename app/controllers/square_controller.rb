b4ae1aba30aa980e1ac4df506743b2b31a8f72b9
Ashanti Hatchett <ahatchett02@gmail.com>
2024-09-24 17:27:12 +0000

    added square root pages

--- app/controllers/square_controller.rb
+++ app/controllers/square_controller.rb
@@ -5,8 +5,19 @@ class SquareController < ApplicationController
 
   def square_results
     @user_num = params.fetch("users_number").to_f
-    @square_results = @user_num ** 2
+    @square = @user_num ** 2
 
     render({ :template => "omnicalc_templates/square_results"} )
+  end
+
+  def square_root_new
+    render({ :template => "omnicalc_templates/square_root_new" })
+  end
+
+  def square_root_results
+    @user_num = params.fetch("users_number").to_f
+    @square_root = Math.sqrt(@user_num)
+
+    render({ :template => "omnicalc_templates/square_root_results" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "square", :action => "square_new" })
 
   get("/square/results", { :controller => "square", :action => "square_results" })
+
+  get("/square_root/new", { :controller => "square", :action => "square_root_new" })
+
+  get("/square_root/results", { :controller => "square", :action => "square_root_results" })
   
 end
